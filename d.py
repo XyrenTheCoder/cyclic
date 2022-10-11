82c42220db52e4b98653d74f57a82ec91d799115
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-10-11 18:01:46 +0800

    Update d.py

--- d.py
+++ d.py
@@ -2,7 +2,7 @@ import os, sys
 from grepfunc import grep
 from datetime import datetime
 
-cwd = os.getcwd()
+idir = os.getcwd()
 dirs = os.listdir(cwd)
 initial = datetime.now()
 
@@ -24,10 +24,44 @@ def welcome():
     else:
         return f"Good morning, {user}!"
 
-def check():
-    result = grep(dirs, "Plugins", w=True, x=True)
-    return result
+#def check():
+#    result = grep(dirs, "Plugins", w=True, x=True)
+#    return result
     
-def main(): ...
-    
-print(check())
+def search(arg):
+    result = grep(dirs, arg, w=True, x=True)
+    count = grep(dirs, arg, w=True, x=True, c=True)
+    return result, count
+
+def clear():
+    os.system("cls") if os.name == "nt" else os.system("clear")
+
+def inc(): c += 1
+     
+def main():
+    global a
+    a = input(">>> ")
+    return a, main()
+   
+while True:
+    if a == "time": print(ctime())
+    elif a == "search":
+        b = input("Search for: ")
+        print(search(b))
+    elif a == "clear": clear()
+    elif a == "cwd": print(os.getcwd())
+    elif a == "debug":
+        c = 0
+        print(f"{c}-{ctime()})
+        inc()
+        print(f"{c}-{search(sys.argv[0])})
+        inc()
+        print(f"{c}-{idir})
+        inc()
+        print(f"{c}-{dirs})
+        inc()
+        print(f"{c}-{starttime})
+        inc()
+        print(f"{c}-{a})
+        #inc()
+    else: print(f"System: Invalid command: {a})
