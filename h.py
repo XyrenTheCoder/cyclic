267ad4e1227146e14e3a7ba2a93a432f0ab285b0
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-10-12 20:42:16 +0800

    Update h.py

--- h.py
+++ h.py
@@ -10,31 +10,41 @@ idir = os.getcwd()
 dirs = os.listdir(idir)
 initial = datetime.now()
 
-#global starttime, starth, startm, starts
+global starttime, starth, startm, starts
 starttime = initial.strftime("%H:%M:%S")
 starth = int(initial.strftime("%H"))
 startm = int(initial.strftime("%M"))
 starts = int(initial.strftime("%S"))
 
 def ctime():
+    global getnow
     getnow = datetime.now()
     t = getnow.strftime("%H:%M:%S")
     return t
 
+def uptime():
+    h = int(datetime.now().strftime("%H"))
+    m = int(datetime.now().strftime("%M"))
+    s = int(datetime.now().strftime("%S"))
+    uh = h - starth
+    um = m - startm
+    us = s - starts
+    return uh, um, us
+    
 def search(arg, flag=None):
     try:
         if flag == None:
             result = grep(dirs, arg)
             count = grep(dirs, arg, c=True)
-        elif flag == "w":
+        elif flag == "w": #exact word
             result = grep(dirs, arg, w=True)
             count = grep(dirs, arg, w=True, c=True)
         elif flag == "i": #ignore case
-            result = grep(dirs, arg, w=True, i=True)
-            count = grep(dirs, arg, w=True, i=True, c=True)
-        elif flag == "x": #exact searching
-            result = grep(dirs, arg, w=True, x=True)
-            count = grep(dirs, arg, w=True, x=True, c=True)
+            result = grep(dirs, arg, i=True)
+            count = grep(dirs, arg, i=True, c=True)
+        elif flag == "x": #exact line
+            result = grep(dirs, arg, x=True)
+            count = grep(dirs, arg, x=True, c=True)
         elif flag == "ix" or flag == "xi":
             result = grep(dirs, arg, x=True, i=True)
             count = grep(dirs, arg, x=True, i=True, c=True)
@@ -44,23 +54,21 @@ def search(arg, flag=None):
         elif flag == "wx" or flag == "xw":
             result = grep(dirs, arg, w=True, x=True)
             count = grep(dirs, arg, w=True, x=True, c=True)
-        elif len(flag) > 2:
-            raise ArgumentError
-        else:
-            raise FlagError
+        elif len(flag) > 2: raise ArgumentError
+        else: raise FlagError
         return result, count
     except ArgumentError: print("Search: ArgumentError: Too many flags.")
     except FlagError: print("Search: FlagError: No such flag(s).")
     
-def clear():
-    os.system("cls") if os.name == "nt" else os.system("clear")
+def clear(): os.system("cls") if os.name == "nt" else os.system("clear")
+
+def exit(): sys.exit(0)
 
 def history():
     f = open("history.txt", "r")
     lines = f.readlines()
     e = []
-    for i in lines:
-        e.append(i)
+    for i in lines: e.append(i)
     return e
     
 def main():
@@ -89,6 +97,7 @@ while True:
         except TypeError: pass
         except MissingRequiredArgument: print("Search: MissingRequiredArgument: Missing 1 or more arguments.")
     elif a == "clear": clear()
+    elif a == "exit": exit()
     elif a == "cwd": print(os.getcwd())
     elif a == "history":
         d = -1
@@ -96,6 +105,9 @@ while True:
         for g in e:
             d += 1
             print(f"{d}    {g.strip()}")
+    elif a == "uptime":
+        uh, um, us = uptime()
+        print(f"{uh}h {um}m {us}s")
     elif a == "debug":
         global c
         c = 0
@@ -110,4 +122,8 @@ while True:
         print(f"{c}-{starttime}")
         c += 1
         print(f"{c}-{a}")
+        c += 1
+        print(f"{c}-{uptime()}")
+        c += 1
+        print(f"{c}-{history()}")
     else: print(f"System: Invalid command: {a}")
