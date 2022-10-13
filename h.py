242810019be62a8e1d3d65d50bd72b98a4cca0f8
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-10-13 05:01:53 +0300

    Update h.py

--- h.py
+++ h.py
@@ -1,37 +1,23 @@
-import os, sysfrom grepfunc import grep
+import os, sys
+from grepfunc import grep
+from datetime import datetime
 
-                result, count = search(tar, flag)
-            print(f"{count} results found:\n"+"\n".join(result))
-        except TypeError: pass
-        except MissingRequiredArgument: print("Search: MissingRequiredArgument: Missing 1 or more arguments.")
-    elif trim == "clear": clear()
-    elif trim == "exit": exit()
-    elif trim == "cwd": print(os.getcwd())
-    elif trim == "history":
-        d = -1
-        e = history()
-        for g in e:
-            d += 1
-            print(f"{d}{(6 - len(str(d))) * ' '} {g.strip()}")
-    elif trim == "uptime":
-        uh, um, us = uptime()
-        print(f"{uh}h {um}m {us}s")
-    elif trim == "debug":
-        global c
-        c = 0
-        print(f"{c}-{ctime()}")
-        c += 1
-        print(f"{c}-{search(sys.argv[0])}")
-        c += 1
-        print(f"{c}-{idir}")
-        c += 1
-        print(f"{c}-{dirs}")
-        c += 1
-        print(f"{c}-{starttime}")
-        c += 1
-        print(f"{c}-{a}")
-        c += 1
-        print(f"{c}-{uptime()}")
-        c += 1
-        print(f"{c}-{history()}")
-    else: print(f"System: Invalid command: {trim}")
+class FlagError(Exception): pass
+class MissingRequiredArgument(Exception): pass
+class ArgumentError(Exception): pass
+    
+idir = os.getcwd()
+dirs = os.listdir(idir)
+initial = datetime.now()
+
+global starttime, starth, startm, starts
+starttime = initial.strftime("%H:%M:%S")
+starth = int(initial.strftime("%H"))
+startm = int(initial.strftime("%M"))
+starts = int(initial.strftime("%S"))
+
+def ctime():
+    global getnow
+    getnow = datetime.now()
+    t = getnow.strftime("%H:%M:%S")
+    return t
